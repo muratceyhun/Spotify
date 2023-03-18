23dfc46b60a79e6ba8db520b940eb3fe3b195c81
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-03-18 16:47:18 +0300

    collectionview has been created

--- Spotify/HomeViewController.swift
+++ Spotify/HomeViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 class HomeViewController: UIViewController {
     
     let menuBar = MenuBar()
+    let playlistCellID = "playlistID"
+    
+    lazy var collectionView: UICollectionView = {
+       let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(PlaylistCell.self, forCellWithReuseIdentifier: playlistCellID)
+        cv.backgroundColor = .spotifyBlack
+        cv.isPagingEnabled = true
+        cv.dataSource = self
+        cv.delegate = self
+        return cv
+    }()
+    
+    let colors: [UIColor] = [.systemRed, .systemBlue, .systemTeal]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +40,39 @@ class HomeViewController: UIViewController {
         menuBar.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(menuBar)
+        view.addSubview(collectionView)
         
+        // menubar
         NSLayoutConstraint.activate([
             menuBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             menuBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             menuBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            menuBar.heightAnchor.constraint(equalToConstant: 42)
+            menuBar.heightAnchor.constraint(equalToConstant: 42),
+            
+            // collection view
+            
+            collectionView.topAnchor.constraint(equalToSystemSpacingBelow: menuBar.bottomAnchor, multiplier: 2),
+            collectionView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 0),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: playlistCellID, for: indexPath) as! PlaylistCell
+        cell.backgroundColor = colors[indexPath.item]
+        return cell
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: collectionView.frame.height)
     }
 }
---
+++ Spotify/PlaylistCell.swift
@@ -0,0 +1,12 @@
+//
+//  PlaylistCell.swift
+//  Spotify
+//
+//  Created by Murat Ceyhun Korpeoglu on 18.03.2023.
+//
+
+import UIKit
+
+class PlaylistCell: UICollectionViewCell {
+    
+}
